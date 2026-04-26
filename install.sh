0ab3145ae8919a7746596cb0a8109cb1003faa89
DanangHaristianto <dananghasristianto9471>
2026-04-27 00:59:14 +0700

    add one line installer

--- install.sh
+++ install.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-echo "=== DEVBOX INSTALLER ==="
+echo "================================="
+echo "      DEVBOX CLI INSTALLER      "
+echo "================================="
 
+echo "[1/4] Update system..."
 pkg update -y && pkg upgrade -y
 
+echo "[2/4] Install tools..."
 pkg install git -y
 pkg install python -y
 pkg install nodejs -y
 pkg install curl -y
 
-echo "DevBox berhasil diinstall!"
+echo "[3/4] Clone project..."
+
+if [ -d "$HOME/devbox-cli" ]; then
+    rm -rf "$HOME/devbox-cli"
+fi
+
+git clone https://github.com/DanangHaristianto/devbox-cli.git $HOME/devbox-cli
+
+echo "[4/4] Setup command..."
+
+chmod +x $HOME/devbox-cli/devbox
+cp $HOME/devbox-cli/devbox $PREFIX/bin/
+
+echo "================================="
+echo " DEVBOX INSTALLED SUCCESS "
+echo " ketik: devbox "
+echo "================================="
